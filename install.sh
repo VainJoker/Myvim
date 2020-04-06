1f6de9e5e5ba7bdefccb0c2243352c8fe85db095
vainjoker <vainjoker@163.com>
2020-04-06 16:34:49 +0800

    readme added and maybe the last version for a long time

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # sudo curl -sL install-node.now.sh/lts | bash
 # curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
-cp $HOME/myvim/_vimrc $HOME/.vimrc
-mkdir $HOME/.config/nvim
-cp  $HOME/myvim/init.vim $HOME/.config/nvim/init.vim
-cp -u -r  $HOME/myvim/coc $HOME/.config/
-vim+PlugInstall
+cp $HOME/MyVim/_vimrc $HOME/.vimrc
+cp  $HOME/MyVim/init.vim $HOME/.config/nvim/init.vim
+cp -u -r  $HOME/MyVim/coc $HOME/.config/
